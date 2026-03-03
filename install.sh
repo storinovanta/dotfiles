17960b0e95ed607237e03cb7bdf7651ea1a539b7
Sam Storino <sam.storino@vanta.com>
2026-03-03 14:22:11 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+# not really needed since I'm not installing additional scripts
+#!/bin/bash
+
+# Needed to default to zsh shell when SSH'ing in
+sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
+
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
+
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
+
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
+}
+
+create_symlinks
